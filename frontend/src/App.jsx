440f66eb1cf6f2d0f71a1b05b56f135b3bd00b64
devparmar16 <devparmar1676@gmail.com>
2025-12-09 21:04:56 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 
-const API_URL = import.meta.env.BACK_API_URL;
+const API_URL = import.meta.env.VITE_API_URL;
 
 function App() {
   const [subject, setSubject] = useState("");
